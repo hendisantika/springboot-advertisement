9e985f0a1ebb9534f64578affb2db96521127bf8
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:35:43 +0700

    #18 - Add modelToEntity in AdvertisementConverter

--- src/main/java/com/hendisantika/component/AdvertisementConverter.java
+++ src/main/java/com/hendisantika/component/AdvertisementConverter.java
@@ -1,8 +1,12 @@
 package com.hendisantika.component;
 
+import com.hendisantika.dto.AdvertisementDTO;
+import com.hendisantika.entity.Advertisement;
 import com.hendisantika.service.PictureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +23,17 @@ public class AdvertisementConverter {
     private PictureService pictureService;
     @Autowired
     private PictureConverter pictureConverter;
+
+    public Advertisement modelToEntity(AdvertisementDTO model) {
+        Advertisement ad = new Advertisement();
+        ad.setId(model.getId());
+        ad.setDescription(model.getDescription());
+        ad.setTypology(model.getTypology());
+        ad.setHouseSize(model.getHouseSize());
+        ad.setGardenSize(model.getGardenSize());
+        ad.setIrrelevantFrom(model.getIrrelevantFrom());
+        ad.setScore(model.getScore());
+        ad.setPictures(model.getPicturesIds().stream().map(pictureService::findEntityById).collect(Collectors.toList()));
+        return ad;
+    }
 }
