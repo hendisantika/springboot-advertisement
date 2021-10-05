3a76d2291199096c9e9ab6112bd54e1a4508eb99
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:52:55 +0700

    #36 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-advertisement
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/21
+ * Time: 06.52
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
