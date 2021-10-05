961edbf54a164ca9e7e3dd5836fb6504e5c44706
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:24:18 +0700

    #6 - Add Advertisement Repository

---
+++ src/main/java/com/hendisantika/repository/AdvertisementRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Advertisement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-advertisement
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/21
+ * Time: 06.23
+ */
+public interface AdvertisementRepository extends JpaRepository<Advertisement, Integer> {
+}
