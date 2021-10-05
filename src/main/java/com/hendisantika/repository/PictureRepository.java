ae73db7cb57f9a8f5879ae9824e385da585f2116
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:24:52 +0700

    #7 - Add PictureRepository

---
+++ src/main/java/com/hendisantika/repository/PictureRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-advertisement
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/21
+ * Time: 06.24
+ */
+public interface PictureRepository extends JpaRepository<Picture, Integer> {
+}
