a44f5e45118dbe874c0ca62bf8f6d8c7aaa7ad65
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:15:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdvertisementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdvertisementApplication.class, args);
+    }
+
+}
