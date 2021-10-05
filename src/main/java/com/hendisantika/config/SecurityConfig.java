9e5f5ac569b58abbcb8fab892448b4c485a8e901
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:53:52 +0700

    #38 - Add configure in SecurityConfig

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.hendisantika.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -24,6 +26,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user").password("{noop}password").roles("USER")
                 .and()
                 .withUser("qualityUser").password("{noop}password").roles("USER", "QUALITY_USER");
+    }
 
+    /**
+     * Securize endpoints with HTTP Basic Authentication
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic() //HTTP Basic authentication
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/advertisement").permitAll() // All users
+                .antMatchers(HttpMethod.GET, "/api/advertisement/**").hasRole("QUALITY_USER") // Only users with
+                // ADMIN role
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
     }
 }
