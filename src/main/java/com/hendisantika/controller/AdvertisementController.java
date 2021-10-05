5e4bb977d6461704678bfa848d2fac4fbe708ba1
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:59:38 +0700

    #45 - Add findAll API in AdvertisementController

--- src/main/java/com/hendisantika/controller/AdvertisementController.java
+++ src/main/java/com/hendisantika/controller/AdvertisementController.java
@@ -1,10 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.AdvertisementDTO;
 import com.hendisantika.dto.AdvertisementScoreDTO;
 import com.hendisantika.service.AdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,5 +30,13 @@ public class AdvertisementController {
     @GetMapping(value = "/advertisement/calculate-scores")
     public List<AdvertisementScoreDTO> calculateScores() {
         return this.advertisementService.calculateScores();
+    }
+
+    @GetMapping(value = "/advertisement/all")
+    public List<AdvertisementDTO> findAll(@RequestParam(required = false, defaultValue = "all") String show,
+                                          @RequestParam(required = false) String sort) {
+        boolean relevants = show.equals("all") || show.equals("relevants");
+        boolean irrelevants = show.equals("all") || show.equals("irrelevants");
+        return this.findAdvertisements(relevants, irrelevants, sort);
     }
 }
