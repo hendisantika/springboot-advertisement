9c59af0523f1fea2e035bd30bd5d667ab2556b1b
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:44:14 +0700

    #29 - Add findAll in AdvertisementService

--- src/main/java/com/hendisantika/service/AdvertisementService.java
+++ src/main/java/com/hendisantika/service/AdvertisementService.java
@@ -1,9 +1,15 @@
 package com.hendisantika.service;
 
 import com.hendisantika.component.AdvertisementConverter;
+import com.hendisantika.component.AdvertisementScoreConverter;
+import com.hendisantika.dto.AdvertisementDTO;
 import com.hendisantika.repository.AdvertisementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,9 +25,22 @@ public class AdvertisementService {
 
     private final static int MIN_AD_SCORE = 40;
     @Autowired
-    private AdvertisementRepository repository;
+    private AdvertisementRepository advertisementRepository;
     @Autowired
     private AdvertisementConverter converter;
     @Autowired
     private AdvertisementScoreConverter adScoreconverter;
+
+    public List<AdvertisementDTO> findAll(Boolean relevants, Boolean irrelevants, String sortField, String sortOrder) {
+        List<AdvertisementDTO> advertisements = advertisementRepository.findAll().stream()
+                .parallel()
+                .filter(ad -> (irrelevants && ad.getScore() < MIN_AD_SCORE) || (relevants && ad.getScore() >= MIN_AD_SCORE))
+                .map(converter::entityToModel)
+                .collect(Collectors.toList());
+        Comparator<AdvertisementDTO> comparator = AdvertisementDTO.getAdComparator(sortField, sortOrder);
+        if (comparator != null) {
+            advertisements.sort(comparator);
+        }
+        return advertisements;
+    }
 }
