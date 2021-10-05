d64cb63f793fcf849d0f124a6724d70aeb693271
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:58:35 +0700

    #44 - Add calculateScores API in AdvertisementController

--- src/main/java/com/hendisantika/controller/AdvertisementController.java
+++ src/main/java/com/hendisantika/controller/AdvertisementController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.AdvertisementScoreDTO;
 import com.hendisantika.service.AdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,9 @@ public class AdvertisementController {
 
     @Autowired
     private AdvertisementService advertisementService;
+
+    @GetMapping(value = "/advertisement/calculate-scores")
+    public List<AdvertisementScoreDTO> calculateScores() {
+        return this.advertisementService.calculateScores();
+    }
 }
