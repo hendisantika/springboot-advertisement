a72887ebb77b74d48d15d49d41f213fc7b787b0e
Hendi Santika <hendisantika@gmail.com>
2021-10-06 06:50:08 +0700

    #34 - Modify ScoresConfig

--- src/main/java/com/hendisantika/dto/ScoresConfig.java
+++ src/main/java/com/hendisantika/dto/ScoresConfig.java
@@ -1,7 +1,13 @@
 package com.hendisantika.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,6 +18,9 @@ import java.util.List;
  * Date: 06/10/21
  * Time: 06.47
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScoresConfig {
     private final List<String> pictures = new ArrayList<>();
     private final List<String> descriptionContains = new ArrayList<>();
@@ -19,4 +28,19 @@ public class ScoresConfig {
     private final List<String> adComplete = new ArrayList<>();
     private Integer noPictures;
     private Integer haveDescription;
+
+    public Map<String, Integer> getPicturesMap() {
+        return this.pictures
+                .stream()
+                .map(element -> element.split(":"))
+                .collect(Collectors.toMap(element -> element[0], element -> Integer.parseInt(element[0])));
+    }
+
+    public Map<String, Integer> getDescriptionContainsMap() {
+        return this.descriptionContains
+                .stream()
+                .map(element -> element.split(":"))
+                .collect(Collectors.toMap(element -> element[0], element -> Integer.parseInt(element[1])));
+    }
+
 }
