80c6cf6224b1756168b1819f8d5691efd1232bbb
Hendi Santika <hendisantika@gmail.com>
2021-10-06 07:08:46 +0700

    #51 - Add ScoreHelperTest

---
+++ src/test/java/com/hendisantika/helper/ScoreHelperTest.java
@@ -0,0 +1,21 @@
+package com.hendisantika.helper;
+
+import com.hendisantika.entity.Advertisement;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-advertisement
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/21
+ * Time: 07.08
+ */
+@SpringBootTest
+public class ScoreHelperTest {
+    private List<Advertisement> advertisements;
+
+}
