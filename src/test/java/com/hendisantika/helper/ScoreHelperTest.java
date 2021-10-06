eecf891089bf68f74961ae2e56788a3e81e3d475
Hendi Santika <hendisantika@gmail.com>
2021-10-06 07:09:17 +0700

    #52 - Add init in ScoreHelperTest

--- src/test/java/com/hendisantika/helper/ScoreHelperTest.java
+++ src/test/java/com/hendisantika/helper/ScoreHelperTest.java
@@ -1,8 +1,12 @@
 package com.hendisantika.helper;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.entity.Advertisement;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -18,4 +22,12 @@ import java.util.List;
 public class ScoreHelperTest {
     private List<Advertisement> advertisements;
 
+    @BeforeEach
+    public void init() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<Advertisement>> typeReference = new TypeReference<List<Advertisement>>() {
+        };
+        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/exampleData.json");
+        this.advertisements = mapper.readValue(inputStream, typeReference);
+    }
 }
